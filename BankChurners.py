f905b8432e807a9f2ef2ea9325cd7fecf325612d
Ibigbami Oluwaseunfunmi <108458150+Funmy-codes@users.noreply.github.com>
2024-10-09 23:11:49 +0100

    Update BankChurners.py

--- BankChurners.py
+++ BankChurners.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 # import seaborn as sns
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 # %pip install h5py graphviz pydot
